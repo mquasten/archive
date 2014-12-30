5a679d6e521dc1309b5a52a06be9dc0b021f19f9
Manfred Quasten <manfred.quasten@arcor.de>
2014-12-30 16:28:16 +0100

    Search

--- archive/src/main/java/de/mq/archive/web/search/SearchPage.java
+++ archive/src/main/java/de/mq/archive/web/search/SearchPage.java
@@ -2,14 +2,18 @@ package de.mq.archive.web.search;
 
 
 
+import java.util.Arrays;
+
 import javax.inject.Inject;
 
 import org.apache.wicket.markup.html.WebPage;
 import org.apache.wicket.markup.html.basic.Label;
 import org.apache.wicket.markup.html.form.Button;
+import org.apache.wicket.markup.html.form.DropDownChoice;
 import org.apache.wicket.markup.html.form.Form;
 import org.apache.wicket.markup.html.form.Radio;
 import org.apache.wicket.markup.html.form.RadioGroup;
+import org.apache.wicket.markup.html.form.TextField;
 import org.apache.wicket.markup.html.list.ListItem;
 import org.apache.wicket.markup.html.list.ListView;
 import org.apache.wicket.model.Model;
@@ -18,6 +22,7 @@ import org.springframework.context.MessageSource;
 
 import de.mq.archive.domain.Archive;
 import de.mq.archive.domain.ArchiveService;
+import de.mq.archive.domain.Category;
 
 
 public class SearchPage extends WebPage {
@@ -30,6 +35,15 @@ public class SearchPage extends WebPage {
 	private MessageSource messageSource;
 
 
+	private final Model<String> searchNameLabelModel = new Model<>();
+	private final Model<String> searchCategoryLabelModel = new Model<>();
+	private final Model<String> searchArchiveLabelModel = new Model<>();
+	
+	private final Model<Category> searchCategoryModel = new Model<>();
+	private final Model<String> searchArchiveModel = new Model<>();
+	private final Model<String> searchCriteriaHeadlineModel = new Model<>();
+	
+	
 	private final Model<String> nameHeaderLabelModel = new Model<>();
 	private final Model<String> categoryHeaderLabelModel = new Model<>();
 	private final Model<String> dateHeaderLabelModel = new Model<>();
@@ -37,14 +51,52 @@ public class SearchPage extends WebPage {
 	private final Model<String> newButtonModel = new Model<>();
 	private final Model<String> changeButtonModel = new Model<>();
 	private final Model<String> showButtonModel = new Model<>();
+	private final Model<String> searchButtonModel = new Model<>();
 	private final Model<String> searchTableHeadlineModel = new Model<>();
 	
 	private final Model<String> applicationHeadlineModel = new Model<>();
 	private final Model<String> pageHeadlineModel = new Model<>();
 	
-	
+	private final Model<String> nameSearchModel = new Model<>(); 
 	
 	public SearchPage(final PageParameters parameters) {
+		
+		
+		
+		final Form<String> searchForm = new Form<>("searchForm");
+		
+		add(searchForm);
+		add(new Label("searchCriteriaHeadline", searchCriteriaHeadlineModel));
+	
+		searchForm.add(new TextField<String>("searchName", nameSearchModel));
+		searchForm.add(new Label("searchNameLabel" , searchNameLabelModel));
+		
+		
+		
+		final DropDownChoice<Category> dropDownChoice = new DropDownChoice<>("searchCategrory", searchCategoryModel, Arrays.asList(Category.values()));
+		dropDownChoice.setNullValid(true);
+	   searchForm.add(dropDownChoice);
+		searchForm.add(new Label("searchCategoryLabel" , searchCategoryLabelModel));
+		
+		searchForm.add(new TextField<String>("searchArchive", searchArchiveModel));
+		searchForm.add(new Label("searchArchiveLabel" , searchArchiveLabelModel));
+		
+		
+		searchForm.add(new Button("searchButton", searchButtonModel) {
+
+			private static final long serialVersionUID = 1L;
+
+			@Override
+			public void onSubmit() {
+				System.out.println(nameSearchModel.getObject());
+				System.out.println(searchCategoryModel.getObject());
+				System.out.println(searchArchiveModel.getObject());
+				super.onSubmit();
+			}
+			
+			
+		});
+		
 		final RadioGroup<Archive> group=new RadioGroup<Archive>("group", new Model<Archive>());
 		final Form<Archive> form = new Form<Archive>("form");
 		final Button newButton = new Button("newButton", newButtonModel);
@@ -91,6 +143,16 @@ public class SearchPage extends WebPage {
 
 	@Override
 	protected void onBeforeRender() {
+		
+		searchCriteriaHeadlineModel.setObject(string("archive_search_search_headline"));
+		searchNameLabelModel.setObject(string("archive_search_search_name"));
+		
+		searchCategoryLabelModel.setObject(string("archive_search_search_category"));
+		
+		searchArchiveLabelModel.setObject(string("archive_search_search_archive"));
+		
+		searchButtonModel.setObject(string("archive_search_search_button"));
+		
 		nameHeaderLabelModel.setObject(string("archive_search_header_name"));
 		
 		categoryHeaderLabelModel.setObject(string("archive_search_header_category"));
