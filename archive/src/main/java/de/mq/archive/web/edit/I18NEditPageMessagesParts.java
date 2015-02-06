e5e31b39178cdeff424f164d3d923171576ccc61
Manfred Quasten <manfred.quasten@arcor.de>
2015-02-06 20:50:59 +0100

    Validierung

--- archive/src/main/java/de/mq/archive/web/edit/I18NEditPageMessagesParts.java
+++ archive/src/main/java/de/mq/archive/web/edit/I18NEditPageMessagesParts.java
@@ -9,9 +9,11 @@ public enum I18NEditPageMessagesParts implements WicketIdAware {
 	
 	DocumentDate("archive_edit_document_date_message"), 
 	Category("archive_edit_mandatory_message"),
-	ArchiveId("archive_edit_mandatory_message"),
-	Name("archive_edit_mandatory_message");
+	ArchiveId("archive_edit_archive_id_message"),
+	Name("archive_edit_name_message");
 	
+	private static final String FEEDBACK = "Feedback";
+	private static final String MESSAGE = "Message";
 	private String key;
 	
 	I18NEditPageMessagesParts( final String key){
@@ -20,11 +22,11 @@ public enum I18NEditPageMessagesParts implements WicketIdAware {
 	
 	@Override
 	public String wicketId() {
-		 return  StringUtils.uncapitalize(name() +"Message");
+		 return  StringUtils.uncapitalize(name() +MESSAGE);
 	}
 	
 	public String wicketIdFeedback() {
-		 return  StringUtils.uncapitalize(name() +"Feedback");
+		 return  StringUtils.uncapitalize(name() +FEEDBACK);
 	}
 	
 	public String wicketIdInput() {
